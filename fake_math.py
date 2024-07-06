284d51514cec2cc47ee0e7ed134861b10cf55bb1
andreyalf1961 <bazulin.a.i@gmail.com>
2024-07-06 18:30:19 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+# импортируемая ф-ция для module_4_1
+def divide(first, second):
+    if second == 0:
+        a = 'Ошибка'
+        return a
+    else:
+        return first / second
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+# импортируемая ф-ция для module_4_1
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
